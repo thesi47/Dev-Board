756c6cbba99e50c78988168bf397156d2797b03c
thesi47 <shawoni510@gmail.com>
2025-12-10 00:49:08 +0600

    Add activity log section to index.html; implement clear history button and notification container; enhance button functionality in disable-buttons.js

--- scripts/disable-buttons.js
+++ scripts/disable-buttons.js
@@ -2,6 +2,17 @@ document.querySelectorAll('.card-button').forEach(button => {
     button.addEventListener('click', function () {
         this.classList.remove('btn-primary');
         this.classList.add('btn-disabled');
+        const counterIncrease = document.getElementById('counter-increase');
+        const counterDecrease = document.getElementById('counter-decrease');
+        let increaseValue = parseInt(counterIncrease.textContent);
+        let decreaseValue = parseInt(counterDecrease.textContent);
+        increaseValue += 1;
+        decreaseValue -= 1;
+        counterIncrease.textContent = increaseValue.toString().padStart(2, '0');
+        counterDecrease.textContent = decreaseValue.toString().padStart(2, '0');
+        if (decreaseValue <= 0) {
+            alert('Congrats, All tasks are completed!');
+        }
     });
 });
 
